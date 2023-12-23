fd23646b0e18fdd1eaab2594046f82bc98e32c7b
deepthummar222 <135607109+deepthummar222@users.noreply.github.com>
2023-12-23 10:07:23 +0530

    Update Sidebar.js

--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -1,75 +1,76 @@
 import { Button, Nav, NavItem } from "reactstrap";
-import { Link, json, useHref, useLocation } from "react-router-dom";
-import { useEffect, useRef, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const navigation = [
   {
     title: "Dashboard",
-    href: "/starter",
+    path: "/starter",
     icon: "bi bi-speedometer2",
   },
   {
     title: "UserData",
-    href: "/data",
+    path: "/data",
     icon: "bi bi-person-workspace ",
   },
-
   {
     title: "Online User",
-    href: "/online",
+    path: "/online",
     icon: "bi bi-people",
   },
   {
     title: "festival list",
-    href: "/favimg",
+    path: "/favimg",
     icon: "bi bi-images",
   },
   {
     title: "Add emojis",
-    href: "/Addemojis",
+    path: "/Addemojis",
     icon: "bi bi-emoji-heart-eyes-fill",
   },
   {
     title: "Add Profile Frems",
-    href: "/AddFrems",
+    path: "/AddFrems",
     icon: "bi bi-app",
   },
-  
   {
     title: "Coin transaction",
-    href: "/coinData",
+    path: "/coinData",
     icon: "bi bi-person-plus-fill",
   },
   {
     title: "CoinValue",
-    href: "/coinValue",
+    path: "/coinValue",
     icon: "bi bi-person-plus-fill",
   },
-
 ];
 
 const Sidebar = () => {
   const showMobilemenu = () => {
     document.getElementById("sidebarArea").classList.toggle('showSidebar');
   };
-  let location = useLocation();
+
+  const navigate = useNavigate();
+  const location = useLocation();
   const [route, setRoute] = useState({ 
     to: location.pathname,
     from: location.pathname 
   });
-  localStorage.setItem("data",JSON.stringify(route))
-  const pathdata= localStorage.getItem("data")
-  const parsedData = JSON.parse(pathdata);
-  useEffect(()=> {
-    setRoute((prev)=> ({to: location.pathname, from: prev.to}) )
-  }, [location]);
 
-      if(parsedData.to == parsedData.from){
-      }
-      else{
-        window.location.reload()
-        console.log("reloaddata")
-      }
+  useEffect(() => {
+  setRoute((prev) => {
+    localStorage.setItem("data", JSON.stringify({ to: location.pathname, from: prev.to }));
+    return { to: location.pathname, from: prev.to };
+  });
+}, [location]);
+
+
+  if (route.to === route.from) {
+  } else {
+    window.location.reload();
+    console.log("reloaddata");
+  }
+
   return (
     <div className="bg-dark">
       <div className="d-flex">
@@ -83,22 +84,21 @@ const Sidebar = () => {
       </div>
       <div className="p-3 mt-2">
         <Nav vertical className="sidebarNav">
-          {navigation.map((navigation, index) => (
+          {navigation.map((navItem, index) => (
             <NavItem key={index} className="sidenav-bg">
-              <Link
-                to={navigation.href}
+              <div
                 className={
-                  location.pathname === navigation.href
+                  location.pathname === navItem.path
                     ? "active nav-link py-3"
                     : "nav-link py-3"
                 }
+                onClick={() => navigate(navItem.path)}
               >
-                <i className={navigation.icon}></i>
-                <span className="ms-3 d-inline-block">{navigation.title}</span>
-              </Link>
+                <i className={navItem.icon}></i>
+                <span className="ms-3 d-inline-block">{navItem.title}</span>
+              </div>
             </NavItem>
           ))}
-
         </Nav>
       </div>
     </div>
